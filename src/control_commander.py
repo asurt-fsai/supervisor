a7178a5c14c3421d8f3bf9284d9e4f5aed81470d
Ali Elganzory <alielganzory@hotmail.com>
2022-06-30 17:05:32 +0200

    Added Autonomous Demo Mission Inspection.

--- src/control_commander.py
+++ src/control_commander.py
@@ -1,4 +1,7 @@
+from typing import Callable
+
 import rospy
+
 from std_msgs.msg import UInt16
 from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
 
@@ -10,18 +13,37 @@ class ControlCommander():
         Issues commands to the control module.
     """
 
+    WHEEL_RADIUS = 0.253
+
+
     def __init__(self) -> None:
         # Commands to the low-level control via the CAN.
-        self.publisher_can_command = rospy.Publisher(rospy.get_param('can_command_topic', '/cmd'), AckermannDriveStamped, queue_size=1)
+        self.publisher_can_command = rospy.Publisher(
+            rospy.get_param('can_command_topic', '/cmd'), 
+            AckermannDriveStamped, 
+            queue_size=1)
         # Commands from the navigation module.
-        self.subscriber_navigation_command = rospy.Subscriber(rospy.get_param('navigation_commnad_topic', 'sub_control_actions'), AckermannDriveStamped, callback=self.__low_level_command)
+        self.subscriber_navigation_command = rospy.Subscriber(
+            rospy.get_param('navigation_commnad_topic', 'sub_control_actions'), 
+            AckermannDriveStamped, 
+            callback=self.__navigation_command_handler)
         # Wheel speeds and steering angle feedback.
-        self.subscriber_wheel_speedsfeedback = rospy.Subscriber(rospy.get_param('can_wheel_speeds_topic', '/ros_can/wheel_speeds'), WheelSpeedsStamped, callback=self.__update_last_wheel_speeds)
+        self.subscriber_wheel_speedsfeedback = rospy.Subscriber(
+            rospy.get_param('can_wheel_speeds_topic', '/ros_can/wheel_speeds'), 
+            WheelSpeedsStamped, 
+            callback=self.__update_last_wheel_speeds)
 
         # State.
         self.last_steering_angle_command = 0
         self.last_wheel_speeds = WheelSpeedsStamped(WheelSpeeds(0, 0, 0, 0, 0))
+        self.middlewares: dict[str, Callable[[AckermannDriveStamped], AckermannDriveStamped]] = {}
 
+
+    def __navigation_command_handler(self, command: AckermannDriveStamped):
+        for middleware in self.middlewares.values():
+            command = middleware(command)
+        self.__low_level_command(command)
+    
 
     def __low_level_command(self, command: AckermannDriveStamped):
         self.last_steering_angle_command = command.drive.steering_angle
@@ -43,7 +65,7 @@ class ControlCommander():
     @property
     def current_speed(self) -> float:
         """
-            Last steering angle feedback in degrees.
+            Last speed feedback in m/s.
         """
         return (
             self.last_wheel_speeds.speeds.lf_speed \
@@ -70,10 +92,23 @@ class ControlCommander():
         self.__low_level_command(cmd)
 
     
-    def set_speed(self, rpm: float):
-        wheel_radius = 0.253
+    def set_speed_rpm(self, rpm: float):
+        speed = rpm * 0.10472 * ControlCommander.WHEEL_RADIUS
+        
+        cmd = AckermannDriveStamped(
+            AckermannDrive(
+                0,
+                0,
+                speed,
+                0,
+                0
+            )
+        )
+        self.__low_level_command(cmd)
 
-        speed = rpm * 0.10472 * wheel_radius
+
+    def set_speed_kph(self, kph: float):
+        speed = kph / 3.6
         
         cmd = AckermannDriveStamped(
             AckermannDrive(
@@ -99,3 +134,19 @@ class ControlCommander():
         )
         self.__low_level_command(cmd)
 
+
+    def add_middleware(self, key: str, middleware: Callable[[AckermannDriveStamped], AckermannDriveStamped]):
+        """
+            A function that transforms the command originating from
+            navigation into a new command.
+        """
+
+        self.middlewares[key] = middleware
+
+    
+    def remove_middleware(self, key: str):
+        """
+            Remove a middleware using its key.
+        """
+
+        self.middlewares.pop(key)
--- src/mission_observer.py
+++ src/mission_observer.py
@@ -1,12 +1,11 @@
-import enum
-from dataclasses import dataclass
 from typing import Callable
 
 import rospy
-from std_msgs.msg import Bool
 
-from asurt_msgs.msg import CanState
-from asurt_msgs.msg import RoadState
+from std_msgs.msg import Bool
+from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
+
+from asurt_msgs.msg import CanState, RoadState
 
 from control_commander import ControlCommander
 
@@ -27,6 +26,7 @@ class MissionObserver():
         self.__control_commander: ControlCommander = control_commmander
         self.__subscriber_mission: rospy.Subscriber = None
         self.__subscriber_road_state: rospy.Subscriber = None
+        self.__last_road_state: RoadState = RoadState(0, 0)
 
         self.__mission_to_checker: dict[int, Callable[[RoadState], bool]] = {
             CanState.AMI_SKIDPAD: self.__is_finished_skidpad,
@@ -38,7 +38,6 @@ class MissionObserver():
         self.__subscriber_mission = rospy.Subscriber(rospy.get_param('/ros_can/state', 'CanState'), CanState, self.__can_state_handler, queue_size=1)
         self.__publisher_mission_status = rospy.Publisher(rospy.get_param('mission_status_topic', '/ros_can/mission_flag'), Bool, queue_size=1)
         self.__publisher_driving_flag = rospy.Publisher(rospy.get_param('driving_flag_topic', '/state_machine/driving_flag'), Bool, queue_size=1)
-        return
 
 
     def __can_state_handler(self, can_state: CanState):
@@ -56,15 +55,29 @@ class MissionObserver():
         previous_state = self.__as_state
         self.__as_state = can_state.as_state
         if previous_state != can_state.AS_DRIVING and can_state.as_state == can_state.AS_DRIVING:
+            # Inspection missions.
             if self.__mission == CanState.AMI_DDT_INSPECTION_A:
                 self.__start_inspection_a()
             elif self.__mission == CanState.AMI_DDT_INSPECTION_B:
                 self.__start_inspection_b()
+            elif self.__mission == CanState.AMI_AUTONOMOUS_DEMO:
+                self.__start_autonomous_demo()
+                self.__subscriber_road_state = rospy.Subscriber(rospy.get_param('road_state_topic', 'RoadState'), RoadState, self.__road_state_autonomous_demo_handler, queue_size=1)
+            # Main missions.
             else:
                 self.__launch_system()
                 self.__is_finished_checker = self.__mission_to_checker[self.__mission]
-                self.__subscriber_road_state = rospy.Subscriber(rospy.get_param('road_state_topic', 'RoadState'), RoadState, self.__is_finished, queue_size=1)
+                self.__subscriber_road_state = rospy.Subscriber(rospy.get_param('road_state_topic', 'RoadState'), RoadState, self.__road_state_handler, queue_size=1)
         
+
+    def __road_state_handler(self, road_state: RoadState):
+        self.__last_road_state = road_state
+
+        
+    def __road_state_handler(self, road_state: RoadState):
+        self.__last_road_state = road_state
+        self.__is_finished(road_state)
+
 
     def __is_finished(self, road_state: RoadState) -> bool:
         is_finished: bool = self.__is_finished_checker(road_state)
@@ -146,12 +159,12 @@ class MissionObserver():
         # Ramp up.
         for i in range(1, 11):
             rospy.sleep(1)
-            self.__control_commander.set_speed(max(i * 25, 200))
+            self.__control_commander.set_speed_rpm(max(i * 25, 200))
         
         # Stop.
         for i in range(1, 6):
             rospy.sleep(1)
-            self.__control_commander.set_speed(min(200 - i * 50, 0))
+            self.__control_commander.set_speed_rpm(min(200 - i * 50, 0))
 
         # Signal mission finish status.
         self.__publisher_mission_status.publish(Bool(True))
@@ -161,13 +174,80 @@ class MissionObserver():
 
     def __start_inspection_b(self):
         # Spin up the speed to 50 rpm.
-        self.__control_commander.set_speed(51)
+        self.__control_commander.set_speed_rpm(51)
         # Until reached.
         while self.__control_commander.current_speed < 50:
             rospy.sleep(0.1)
         # Buffer.
         rospy.sleep(1)
 
+        # Trigger EBS system.
+        # Signal mission finish status.
+        self.__publisher_mission_status.publish(Bool(True))
+        if self.__mission_finished_callback is not None:
+            self.__mission_finished_callback()
+
+    
+    def __start_autonomous_demo(self):
+        ###############################
+        # Steer left and right, then forward.
+        ###############################
+
+        # Steer left.
+        self.__control_commander.set_steering_angle(-27.2)
+        rospy.sleep(1)
+        # Steer right.
+        self.__control_commander.set_steering_angle(27.2)
+        rospy.sleep(2)
+        # Steer to straight ahead position.
+        self.__control_commander.set_steering_angle(0)
+        rospy.sleep(1.5)
+
+        ###############################
+        # Launch system to stay in track.
+        # Add middlware to adjust the navigation
+        # commands according to the demo mission.
+        ###############################
+
+        def adjust_speed(command: AckermannDriveStamped, speed) -> AckermannDriveStamped:
+            command = AckermannDriveStamped(
+                AckermannDrive(
+                    command.drive.steering_angle,
+                    0,
+                    speed,
+                    0,
+                    0
+                )
+            )
+            return command
+
+        # Accelerate.
+        self.__control_commander.add_middleware("accelerate", lambda x: adjust_speed(x, 16 * 3.6))
+        self.__launch_system()
+
+        # For 10 meters.
+        while self.__last_road_state.distance < 10:
+            rospy.sleep(0.05)
+        
+        # Decelerate.
+        self.__control_commander.add_middleware("decelerate", lambda x: adjust_speed(x, 0))
+        self.__control_commander.remove_middleware("accelerate")
+
+        # Until stopped.
+        while self.__control_commander.current_speed > 0.1:
+            rospy.sleep(0.1)
+        # Buffer.
+        rospy.sleep(2)
+
+        # Accelerate.
+        start_distance = self.__last_road_state.distance
+        self.__control_commander.add_middleware("accelerate", lambda x: adjust_speed(x, 16 * 3.6))
+        self.__control_commander.remove_middleware("decelerate")
+
+        # For 10 meters.
+        while (self.__last_road_state.distance - start_distance) < 10:
+            rospy.sleep(0.05)
+        
         # Trigger EBS system.
         # Signal mission finish status.
         self.__publisher_mission_status.publish(Bool(True))
